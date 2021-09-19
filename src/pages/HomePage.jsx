84552b1dac83f894120190baab219b6836d63b22
Liuk Tulawie <liukjhivran@gmail.com>
2021-09-19 22:21:38 +0800

    Redirect to login page for demo purposes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useEffect } from 'react';
 
-const HomePage = () => {
+const HomePage = ({ history }) => {
+  useEffect(() => {
+    history.push('login');
+  }, []);
   return <div>HomePage</div>;
 };
 
